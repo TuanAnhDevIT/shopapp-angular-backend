182bfbc3f214a1d8246c80a633116fcd4ed155fb
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-10-08 17:20:34 +0700

    feat(): setup Spring security

--- shopapp/src/main/java/com/project/shopapp/services/UserService.java
+++ shopapp/src/main/java/com/project/shopapp/services/UserService.java
@@ -1,5 +1,6 @@
 package com.project.shopapp.services;
 
+import com.project.shopapp.components.JwtTokenUtil;
 import com.project.shopapp.dtos.UserDTO;
 import com.project.shopapp.exceptions.DataNotFoundException;
 import com.project.shopapp.models.Role;
@@ -9,19 +10,28 @@ import com.project.shopapp.repositories.UserRepository;
 import com.project.shopapp.services.IUserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.sql.Date;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
 public class UserService implements IUserService {
 
-    private UserRepository userRepository;
-    private RoleRepository roleRepository;
+    private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenUtil jwtTokenUtil;
+    private final AuthenticationManager authenticationManager;
 
     @Override
     public User createUser(UserDTO userDTO) throws DataNotFoundException {
+        //register user
         String phoneNumber = userDTO.getPhoneNumber();
         //Kiểm tra số điện thoại đã tồn tại hay chưa
         if (userRepository.existsByPhoneNumber(phoneNumber)){
@@ -44,17 +54,31 @@ public class UserService implements IUserService {
         //Kiểm tra nếu có UserId => không yêu cầu password
         if(userDTO.getFacebookAccountId() == 0 && userDTO.getGoogleAccountId() == 0){
             String password = userDTO.getPassword();
-//            String encodedPassword = passwordEncoder.encode(password);
-            // thuộc về phần spring security
-//            newUser.setPassword(encodedPassword);
+            String encodedPassword = passwordEncoder.encode(password);
+            newUser.setPassword(encodedPassword);
         }
         return userRepository.save(newUser);
     }
 
     @Override
-    public String login(String phoneNumber, String password) {
-        // Đoạn này liên quan đến security, sẽ làm trong phần security
-
-        return null;
+    public String login(String phoneNumber, String password) throws Exception {
+        Optional<User> optionalUser = userRepository.findByPhoneNumber(phoneNumber);
+        if(optionalUser.isEmpty()){
+            throw new DataNotFoundException("Invalid phone number / password");
+        }
+        //return optionalUser.get();//Muốn trả về JWT TOKEN??
+        User existingUser = optionalUser.get();
+        //check password
+        if(existingUser.getFacebookAccountId() == 0 && existingUser.getGoogleAccountId() == 0) {
+            if(!passwordEncoder.matches(password, existingUser.getPassword())){
+                throw new BadCredentialsException("Wrong phone number or password");
+            }
+        }
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                phoneNumber,password
+        );
+        //authenticate with Java Spring Security
+        authenticationManager.authenticate(authenticationToken);
+        return jwtTokenUtil.generateToken(existingUser);
     }
 }
