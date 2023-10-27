0e9f30213ff34d1e3e659a26376550ff42472323
DangTranTuanAnh <dangtrantuananh.2002@gmail.com>
2023-10-27 11:31:57 +0700

    fix(): nothing

--- shopapp/src/main/java/com/project/shopapp/models/BaseEntity.java
+++ shopapp/src/main/java/com/project/shopapp/models/BaseEntity.java
@@ -1,6 +1,6 @@
 package com.project.shopapp.models;
 
-
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Column;
 import jakarta.persistence.MappedSuperclass;
 import jakarta.persistence.PrePersist;
@@ -9,13 +9,14 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
-@Data //toString
+@Data//toString
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @MappedSuperclass
-public class BaseEntity {
+
+public class BaseEntity{
     @Column(name = "created_at")
     private LocalDateTime createdAt;
 
@@ -23,13 +24,13 @@ public class BaseEntity {
     private LocalDateTime updatedAt;
 
     @PrePersist
-    protected void onCreate(){
+    protected void onCreate() {
         createdAt = LocalDateTime.now();
         updatedAt = LocalDateTime.now();
     }
 
     @PreUpdate
-    protected void onUpdate(){
+    protected void onUpdate() {
         updatedAt = LocalDateTime.now();
     }
 
